1f59a9953a10454f0b46b432e6c2168f91c68689
heidi valles <hvalles@ocad.ca>
2012-12-19 09:44:34 -0500

    Emptying master dir, should be populated via branch

--- searchform.php
+++
@@ -1,5 +0,0 @@
-<form method="get" id="searchform" action="">
-	<h3><label class="screen-reader-text" for="s">Site Search</label></h3>
-	<input type="text" value="" name="s" id="s">
-	<input type="submit" id="searchsubmit" value="Go">
-</form>
