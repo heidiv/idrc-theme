4de42b1df8db8ff53463fbf309e2f7fbd05bb15b
heidi valles <hvalles@ocad.ca>
2012-12-19 10:17:10 -0500

    IDI-108: Force update

--- banner.php
+++ banner.php
@@ -4,4 +4,4 @@
 				<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a></h1>				
 				<h2 class="tagline"><a href="http://ocad.ca"><?php bloginfo('description'); ?></a></h2>
 			</div>		
-		</div>
+		</div> 
